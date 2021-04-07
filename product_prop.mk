7e3ca19f76216625a343b7e8a608c467fdd5f57c
ksrt12 <ksrt12group@gmail.com>
2021-04-07 16:19:59 +0300

    tmp: enable adb by default
    Signed-off-by: ksrt12 <ksrt12group@gmail.com>

--- product_prop.mk
+++ product_prop.mk
@@ -5,13 +5,13 @@
 #
 
 # Adb
-ifeq ($(TARGET_BUILD_VARIANT),eng)
+#ifeq ($(TARGET_BUILD_VARIANT),eng)
 # /vendor/default.prop is force-setting ro.adb.secure=1
 # Get rid of that by overriding it in /product on eng builds
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.secure=0 \
     ro.adb.secure=0
-endif
+#endif
 
 # Media
 PRODUCT_PRODUCT_PROPERTIES += \
