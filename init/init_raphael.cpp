360a339031ea70f16a99b581c807d0c3a6ac0a9d
ksrt12 <ksrt12group@gmail.com>
2021-08-04 23:40:15 +0300

    update fingerprint to august

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -61,21 +61,18 @@ void load_raphaelglobal() {
     property_override("ro.product.model", "Mi 9T Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
 }
 
 void load_raphaelin() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphaelin");
     property_override("ro.product.device", "raphaelin");
-    property_override("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
 }
 
 void load_raphael() {
     property_override("ro.product.model", "Redmi K20 Pro");
     property_override("ro.build.product", "raphael");
     property_override("ro.product.device", "raphael");
-    property_override("ro.build.description", "redfin-user 11 RQ3A.210705.001 7380771 release-keys");
 }
 
 
@@ -91,6 +88,7 @@ void vendor_load_properties() {
     } else {
         LOG(ERROR) << __func__ << ": unexcepted region!";
     }
-    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ3A.210705.001/7380771:user/release-keys");
+    property_override("ro.build.fingerprint", "google/redfin/redfin:11/RQ3A.210805.001.A1/7474174:user/release-keys");
+    property_override("ro.build.description", "redfin-user 11 RQ3A.210805.001.A1 7474174 release-keys");
     load_dalvik_properties();
 }
